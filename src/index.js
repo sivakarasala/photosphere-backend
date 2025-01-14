f174f73742b5f21b11ff4a8be40db9030abce046
Siva Karasala <shivataraya@gmail.com>
2025-01-14 16:07:29 +0530

    Refactored the main file to wrap the REST API startup in an async function. This will make it easier to add asynchronous database initialization.

--- src/index.js
+++ src/index.js
@@ -1,40 +1,49 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
-const app = express();
 
 const PORT = process.env.PORT;
 if (!PORT) {
     throw new Error(`Set environment variable PORT.`);
 }
 
-app.post("/asset", (req, res) => {
-    const fileName = req.headers["file-name"];
-    const localFileName = path.join(__dirname, "../uploads", fileName);
-    const fileWriteStream = fs.createWriteStream(localFileName);
-    req.pipe(fileWriteStream)
-        .on("error", err => {
-            console.error(`Error writing ${localFileName}`);
-            console.error(err);
-            res.sendStatus(500);
-        })
-        .on("finish", () => {
-            console.log(`Done writing ${localFileName}`);
-            res.sendStatus(200);
-        });
-});
-
-app.get("/asset", (req, res) => {
-    const fileName = req.query.fileName;
-    const localFileName = path.join(__dirname, "../uploads", fileName);
-    res.writeHead(200, {
-        "Content-Type": "image/png",
+async function main() {
+    const app = express();
+    app.post("/asset", (req, res) => {
+        const fileName = req.headers["file-name"];
+        const localFileName = path.join(__dirname, "../uploads", fileName);
+        const fileWriteStream = fs.createWriteStream(localFileName);
+        req.pipe(fileWriteStream)
+            .on("error", err => {
+                console.error(`Error writing ${localFileName}`);
+                console.error(err);
+                res.sendStatus(500);
+            })
+            .on("finish", () => {
+                console.log(`Done writing ${localFileName}`);
+                res.sendStatus(200);
+            });
     });
+    
+    app.get("/asset", (req, res) => {
+        const fileName = req.query.fileName;
+        const localFileName = path.join(__dirname, "../uploads", fileName);
+        res.writeHead(200, {
+            "Content-Type": "image/png",
+        });
+    
+        const fileReadStream = fs.createReadStream(localFileName);
+        fileReadStream.pipe(res);
+    });
+    
+    app.listen(PORT, () => {
+        console.log(`Server listening on port ${PORT}`);
+    });
+}
 
-    const fileReadStream = fs.createReadStream(localFileName);
-    fileReadStream.pipe(res);
-});
-
-app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
-});
+main()
+    .catch(err => {
+        console.error(`Something went wrong.`);
+        console.error(err);
+        process.exit(1);
+    });
